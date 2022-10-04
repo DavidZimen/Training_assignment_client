0b9b658228b70a6f96b59f2f6137870badb1bec5
DavidZimen <david.zimen1@gmail.com>
2022-10-04 12:58:17 +0200

    Documentation

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -8,8 +8,11 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AddUpdateDialogComponent } from '../../dialogs/add-update-dialog/add-update-dialog.component';
 import { Observable } from 'rxjs';
 import { ConfDialogComponent } from 'src/app/dialogs/conf-dialog/conf-dialog.component';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
+/**
+ * 
+ */
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
@@ -28,7 +31,9 @@ export class UserListComponent implements OnInit {
 
   /**
    * Constructor for UserListComponent.
-   * @param userService - service which communicates with Spring boot backend through HttpClient
+   * @param userService Service which communicates with Spring boot backend through HttpClient
+   * @param addDialog Dialog for adding or updating users.
+   * @param router Router to navigate between components.
    */
   constructor(
     private userService: UserService, 
@@ -50,7 +55,9 @@ export class UserListComponent implements OnInit {
     });
   }
 
-
+  /**
+   * Method to retrieve users from backend through userService.
+   */
   loadUsers(): void {
     this.userService.findUsers().subscribe(response => {
       this.users.data = response;
@@ -64,6 +71,10 @@ export class UserListComponent implements OnInit {
     this.router.navigate(['user-info', id]);
   }
 
+  /**
+   * Opens AddUpdateDialogComponent with blank fields.
+   * Triggered when clicked on the addButton.
+   */
   openAddDialog(): void {
     const dialogConfig = new MatDialogConfig();
 
@@ -78,6 +89,11 @@ export class UserListComponent implements OnInit {
     this.addDialog.open(AddUpdateDialogComponent, dialogConfig);
   }
 
+  /**
+   * Opens AddUpdateDialogComponent with prefilled data based on the userId.
+   * Triggered when clicked on updateButton in the last column of the table.
+   * @param userId Id of a selected user from the table.
+   */
   openUpdateDialog(userId: number): void {
     const dialogConfig = new MatDialogConfig();
 
@@ -92,6 +108,11 @@ export class UserListComponent implements OnInit {
     this.addDialog.open(AddUpdateDialogComponent, dialogConfig);
   }
 
+  /**
+   * Opens ConfDialogComponent to warn user he is about to delete from the database.
+   * Triggered when clicked on deleteButton in the last column of the table.
+   * @param userId Id of the user which is going to be deleted.
+   */
   onDeleteButton(userId: number): void {
     const dialogConfig = new MatDialogConfig();
 
@@ -105,10 +126,12 @@ export class UserListComponent implements OnInit {
     this.addDialog.open(ConfDialogComponent, dialogConfig);
   }
 
+  /**
+   * Filtering the table based on the criteria.
+   * @param event 
+   */
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.users.filter = filterValue.trim().toLowerCase();
-}
-
-  doNothing(): void { }
+  }
 }
--- src/app/dialogs/add-update-dialog/add-update-dialog.component.ts
+++ src/app/dialogs/add-update-dialog/add-update-dialog.component.ts
@@ -4,33 +4,30 @@ import { Inject } from '@angular/core';
 import { UserService } from 'src/app/service/user-service.service';
 import { UserRequest } from 'src/app/user/user-request';
 import { User } from 'src/app/user/user';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { TranslateService } from '@ngx-translate/core';
 
-export const APP_DATE_FORMATS = {
-  parse: {
-      dateInput: 'DD.MM.YYYY',
-  },
-  display: {
-      dateInput: 'DD.MM.YYYY',
-      monthYearLabel: 'MMMM YYYY',
-      dateA11yLabel: 'LL',
-      monthYearA11yLabel: 'MMMM YYYY'}
-}
-
+/**
+ * Component that displays a dialog for adding or updating users.
+ */
 @Component({
   selector: 'app-add-dialog',
   templateUrl: './add-update-dialog.component.html',
-  styleUrls: ['./add-update-dialog.component.css'],
-  providers: [{ provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS }]
+  styleUrls: ['./add-update-dialog.component.css']
 })
 export class AddUpdateDialogComponent implements OnInit {
 
   userRequest: UserRequest = new UserRequest();
   updateUser!: User;
-
   add: boolean;
 
+  /**
+   * Constructor for AddUpdateDialogComponent.
+   * When triggered, it gets correct user from database.
+   * @param dialogRef Reference to the MatDialog.
+   * @param data Data sent from the UserListComponent to retrieve the correct user from server.
+   * @param userService Service which communicates with Spring boot backend through HttpClient.
+   * @param translateService Used to translate the text in the alert dialog.
+   */
   constructor(
       private dialogRef: MatDialogRef<AddUpdateDialogComponent>,
       @Inject(MAT_DIALOG_DATA) public data: any,
@@ -41,7 +38,6 @@ export class AddUpdateDialogComponent implements OnInit {
         if (data.updateUserId !== -1) {
           this.userService.findUserById(data.updateUserId).subscribe(user => {
             this.updateUser = user;
-            console.log(user.version);
           });
         }
   }
@@ -53,12 +49,30 @@ export class AddUpdateDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  onSubmit(): void {
-    this.userService.addUser(this.userRequest).subscribe(data => {});
+  close(): void {
     this.dialogRef.close();
-    this.userService.findUsers();
   }
 
+  /**
+   * Submits the new user to the server, closes the dialog.
+   */
+  onSubmit(): void {
+    this.userService.addUser(this.userRequest).subscribe({
+      next: (data) => {
+        this.dialogRef.close();
+        this.userService.findUsers();
+      },
+      error: (e) => {
+        alert(e);
+      },
+      complete: () => {}
+    });
+  }
+
+  /**
+   * Submits the changes made to updatedUser to the server.
+   * If error is encountered, it shows allert dialog.
+   */
   onSubmitUpdate(): void {
     if (this.updateUser !== undefined) {
       this.userService.updateUser(this.updateUser).subscribe({
@@ -73,9 +87,5 @@ export class AddUpdateDialogComponent implements OnInit {
         complete: () => {}
       });
     }
-  }
-
-  close(): void {
-    this.dialogRef.close();
   }
 }
